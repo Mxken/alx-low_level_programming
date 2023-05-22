47109b9fa25048a9d26b0b4194c983341c3fa2cf
Mthokozisi <mikewillmakedem@gmail.com>
2023-05-22 14:18:51 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(void)
-{
+/**
+ *entry point | main
+ *
+ * Return: Always 0 
+ */
+
+ 
+    {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-}
+    }  
